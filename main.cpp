a39ea13f96a8a011d5fdd61ca90956313ea9f27c
Василий Почкаенко <pochkaenkov@yandex.ru>
2017-05-19 13:06:54 +0700

    Сделал простое переписывание картинок в каталог результата.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QDir>
+#include <QImage>
 #include <QString>
 #include <QStringList>
 
-void AddTextWatermark(const QStringList &fileList) {
-	for (auto name: fileList) {
-		qDebug() << name << endl;
+void AddTextWatermark(const QString &dirPath, const QString &resDirPath) {
+	QDir directory(dirPath);
+	QDir resDir(resDirPath);
+	if(!resDir.exists()) {
+		if(!resDir.mkpath(resDirPath)) {
+			qCritical() << QObject::trUtf8("Не могу создать каталог %0").arg(resDirPath);
+			return;
+		}
+	}
+	auto fileList = directory.entryInfoList({"*.jpg"});
+	for (auto file: fileList) {
+		qDebug() << file.absoluteFilePath() << endl;
+		QImage image(file.absoluteFilePath());
+		auto resFileName = resDir.filePath(file.fileName());
+		if(!image.save(resFileName)) {
+			qCritical() << QObject::trUtf8("Не могу сохранить файл %0").arg(resFileName);
+		}
 	}
 }
 
 int main(int argc, char *argv[]) {
-	QDir directory("D:/test images");
-	auto fileList = directory.entryList({"*.jpg"});
-	AddTextWatermark(fileList);
+	AddTextWatermark("D:/test images", "D:/test images result");
 }
