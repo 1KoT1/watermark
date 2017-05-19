e3d77158cd0d0ce7365b9280b502d453474695ed
Василий Почкаенко <pochkaenkov@yandex.ru>
2017-05-19 15:28:06 +0700

    Рефакторинг: вынес обработку одного изображения в отдельную функцию.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,30 @@
 #include <QPointF>
 #include <QString>
 #include <QStringList>
+#include <exception>
 
-void AddTextWatermark(const QString &dirPath, const QString &resDirPath) {
+class FileSavingFailed : public std::runtime_error {
+public:
+	FileSavingFailed (const QString &fileName) : std::runtime_error("File saving failed."), _fileName(fileName) {}
+	const QString &fileName() { return _fileName; }
+private:
+	QString _fileName;
+};
+
+void AddTextWatermarkToImage(const QFileInfo &file, const QDir &resDir) {
+	qDebug() << file.absoluteFilePath() << endl;
+	QImage image(file.absoluteFilePath());
+	QPainter p(&image);
+	p.setPen(QPen(Qt::white));
+	p.setFont(QFont("Times", 10));
+	p.drawText(0, 10, QObject::trUtf8("Zarubenko Julia | https://vk.com/phzarubenkoiuliia |"));
+	auto resFileName = resDir.filePath(file.fileName());
+	if(!image.save(resFileName)) {
+		throw FileSavingFailed(resFileName);
+	}
+}
+
+void AddTextWatermarkToImageList(const QString &dirPath, const QString &resDirPath) {
 	QDir directory(dirPath);
 	QDir resDir(resDirPath);
 	if(!resDir.exists()) {
@@ -18,21 +40,16 @@ void AddTextWatermark(const QString &dirPath, const QString &resDirPath) {
 	}
 	auto fileList = directory.entryInfoList({"*.jpg"});
 	for (auto file: fileList) {
-		qDebug() << file.absoluteFilePath() << endl;
-		QImage image(file.absoluteFilePath());
-		QPainter p(&image);
-		p.setPen(QPen(Qt::white));
-		p.setFont(QFont("Times", 10));
-		p.drawText(0, 10, QObject::trUtf8("Zarubenko Julia | https://vk.com/phzarubenkoiuliia |"));
-		auto resFileName = resDir.filePath(file.fileName());
-		if(!image.save(resFileName)) {
-			qCritical() << QObject::trUtf8("Не могу сохранить файл %0").arg(resFileName);
-			return;
-		}
+		AddTextWatermarkToImage(file, resDir);
 	}
 }
 
 int main(int argc, char *argv[]) {
 	QGuiApplication a(argc, argv);
-	AddTextWatermark("D:/test images", "D:/test images result");
+	try
+	{
+		AddTextWatermarkToImageList("D:/test images", "D:/test images result");
+	} catch (FileSavingFailed ex) {
+		qCritical() << QObject::trUtf8("Не могу сохранить файл %0").arg(ex.fileName());
+	}
 }
