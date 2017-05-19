2c0bbd5d1d69e3bd69c1c7aba8eba2b65451706d
Василий Почкаенко <pochkaenkov@yandex.ru>
2017-05-19 14:58:08 +0700

    Простое добавление текста, без оформления.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-#include <QCoreApplication>
+#include <QGuiApplication>
 #include <QDebug>
 #include <QDir>
 #include <QImage>
+#include <QPainter>
+#include <QPointF>
 #include <QString>
 #include <QStringList>
 
@@ -10,7 +12,7 @@ void AddTextWatermark(const QString &dirPath, const QString &resDirPath) {
 	QDir resDir(resDirPath);
 	if(!resDir.exists()) {
 		if(!resDir.mkpath(resDirPath)) {
-			qCritical() << QObject::trUtf8("Не могу создать каталог %0").arg(resDirPath);
+			qCritical() << QObject::trUtf8("Не могу создать каталог %0").arg(resDirPath) << endl;
 			return;
 		}
 	}
@@ -18,13 +20,19 @@ void AddTextWatermark(const QString &dirPath, const QString &resDirPath) {
 	for (auto file: fileList) {
 		qDebug() << file.absoluteFilePath() << endl;
 		QImage image(file.absoluteFilePath());
+		QPainter p(&image);
+		p.setPen(QPen(Qt::white));
+		p.setFont(QFont("Times", 10));
+		p.drawText(0, 10, QObject::trUtf8("Zarubenko Julia | https://vk.com/phzarubenkoiuliia |"));
 		auto resFileName = resDir.filePath(file.fileName());
 		if(!image.save(resFileName)) {
 			qCritical() << QObject::trUtf8("Не могу сохранить файл %0").arg(resFileName);
+			return;
 		}
 	}
 }
 
 int main(int argc, char *argv[]) {
+	QGuiApplication a(argc, argv);
 	AddTextWatermark("D:/test images", "D:/test images result");
 }
